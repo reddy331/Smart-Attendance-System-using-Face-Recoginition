559fbce7fcefebdbc4b27514c4b6fbacaff5b3da
Rohith Reddy <117523592+reddy331@users.noreply.github.com>
2024-12-01 22:13:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
-import face_recognition
-import cv2
-import pickle
 import os
+import pickle
+import numpy as np
+import cv2
+import face_recognition
+import cvzone
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from firebase_admin import  storage
+from firebase_admin import storage
+import numpy as np
+from datetime import datetime
  
-#cred = credentials.Certificate("serviceAccountKey.json")
-#firebase_admin.initialize_app(cred, {
-   # 'databaseURL': "",
-   # 'storageBucket': ""
-#})
- 
- 
-# Importing student images
-folderPath = 'Images'
-pathList = os.listdir(folderPath)
-print(pathList)
-imgList = []
-studentIds = []
-for path in pathList:
-    imgList.append(cv2.imread(os.path.join(folderPath, path)))
-    studentIds.append(os.path.splitext(path)[0])
- 
-    #fileName = f'{folderPath}/{path}'
-    #bucket = storage.bucket()
-    #blob = bucket.blob(fileName)
-    #blob.upload_from_filename(fileName)
- 
- 
-    # print(path)
-    #print(os.path.splitext(path)[0])
-print(studentIds)
- 
- 
-def findEncodings(imagesList):
-    encodeList = []
-    for img in imagesList:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
- 
-    return encodeList
- 
- 
-print("Encoding Started ...")
-encodeListKnown = findEncodings(imgList)
-encodeListKnownWithIds = [encodeListKnown, studentIds]
-print("Encoding Complete")
- 
-file = open("EncodeFile.p", 'wb')
-pickle.dump(encodeListKnownWithIds, file)
+cap = cv2.VideoCapture(1)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+ # Importing the mode images into a list
+
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+#print(modePathList)
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+#print(len(imgModeList))
+
+#importing encoding file
+
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
 file.close()
-print("File Saved")
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
+ 
+modeType = 0
+counter = 0
+id = -1
+imgStudent = []
+
+
+while True:
+    success, img = cap.read()
+ 
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+ 
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
